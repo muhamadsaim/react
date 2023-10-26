58579f356eae1b1cc3ebe4e477530f6b53bb80b4
muhamaadsaim <saimrao849@gmail.com>
2023-10-26 15:03:44 +0500

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          saim is {count}
+          qasim is love {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
